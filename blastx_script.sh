40ac2ceeb0648e56b79ca0ff5a5edc19d6026e61
Sourakhata Tirera <tirerasoura@gmail.com>
2022-03-10 17:28:33 -0300

    Delete blastx_script.sh

--- blastx_script.sh
+++
@@ -1,1 +0,0 @@
-/home/stirera/tools/ncbi-blast-2.12.0+/bin/blastx -query NODE_r2_2.fasta -db /home/stirera/work_directory/db/ncbi/nr/nr -num_threads 28 -evalue 0.01 -culling_limit 10 -num_alignments 10 -outfmt "6 qseqid sacc pident length mismatch gaps qstart qend sstart send evalue bitscore staxid sskingdom sframe qframe qlen qseq sseq" -out NODE_r2_2.1.bx.vir.csv
