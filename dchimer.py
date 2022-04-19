cff9f4b121eb2703ee3c693368565ebcf5887a2f
Sourakhata Tirera <tirerasoura@gmail.com>
2022-04-19 14:09:31 -0300

    Update dchimer.py

--- dchimer.py
+++ dchimer.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
 # -*-coding:Latin-1 -*
 
+
+"""
+Licence header
+--------------
+dchimer : python3 main program of d-chimer pipeline.
+Copyright (C) 2022, INSTITUT PASTEUR DE LA GUYANE
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/>.
+date: 19.04.2022
+"""
+
 import os
 import sys
 
