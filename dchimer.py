df73387019d4f6a0005b86670f653483a7d552eb
Sourakhata Tirera <tirerasoura@gmail.com>
2022-05-30 16:25:40 -0300

    Update dchimer.py

--- dchimer.py
+++ dchimer.py
@@ -16,6 +16,10 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program.
 If not, see <https://www.gnu.org/licenses/>.
 date: 19.04.2022
+--
+Author : Sourakhata Tirera <stirera@pasteur-cayenne.fr>
+Author : Jean-Marc Frigerio <jean-marc.frigerio@inrae.fr>
+Author : Alix de Thoisy <alixdet@protonmail.com>
 """
 
 import os
