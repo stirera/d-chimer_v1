9249041597bd291d7cf35cf6239a566ce1942fd0
Sourakhata Tirera <tirerasoura@gmail.com>
2022-06-02 10:24:47 -0300

    Update dchimer_methods.py

--- dchimer_methods.py
+++ dchimer_methods.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 # -*-coding:Latin-1 -*
 
+
 """
 Licence header
 --------------
-dchimer_methods : python3 library of all d-chimer pipeline's generic methods.
+dchimer : python3 main program of d-chimer pipeline.
 Copyright (C) 2022, INSTITUT PASTEUR DE LA GUYANE
 This program is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the Free Software Foundation,
@@ -15,6 +16,10 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with this program.
 If not, see <https://www.gnu.org/licenses/>.
 date: 19.04.2022
+--
+Author : Sourakhata Tirera <stirera@pasteur-cayenne.fr>
+Author : Jean-Marc Frigerio <jean-marc.frigerio@inrae.fr>
+Author : Alix de Thoisy <alixdet@protonmail.com>
 """
 
 import os
