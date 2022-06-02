b97cdc4d333092f7e0c3a3b1b200b40e213618fd
Sourakhata Tirera <tirerasoura@gmail.com>
2022-06-02 10:22:40 -0300

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-pip install biopython
-pip install PyYAML
+pip3 install biopython
+pip3 install PyYAML
 wget "https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/new_taxdump/new_taxdump.tar.gz"
 tar xzf new_taxdump.tar.gz
 cat fullnamelineage.dmp|sed "s/\s\+\|\s\+//g"|sed 's/\|//2g'|awk 'BEGIN{FS="|"}{print $1,$3,$4,$2}'|sed "s/\s/\t/g"|sort -k1,1 > fullnamelineage_taxid_sorted.dmp
